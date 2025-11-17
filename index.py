8f63c0dbe888e1042fd088c553359f2ab8ab4e00
Kaveri-2025 <kaveri17vs@gmail.com>
2025-11-17 16:56:40 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
 print("Hello Kaveri, this is my first GitHub push!")
+def greet(name):
+    return f"Hello {name}, welcome to GitHub!"
+
+if __name__ == "__main__":
+    user = "Kaveri"
+    print(greet(user))
